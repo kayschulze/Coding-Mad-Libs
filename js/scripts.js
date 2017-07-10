0295d747c0c2a864fb6012f4313803917b49bef1
Kim Schulze and Michael Woldemedihin <michael@>
2017-07-10 14:07:04 -0700

    make the code more DRY by using array to store user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     console.log("form submitted.");
-    var person1 = $("input#person1").val();
-    var person2 = $("input#person2").val();
-    var codinglanguage = $("input#codinglanguage").val();
-    var exclamation = $("input#exclamation").val();
-    var verb = $("input#verb").val();
-    var noun = $("input#noun").val();
+    var blanks = ["person1", "person2", "codinglanguage", "exclamation", "verb", "noun"];
+    blanks.forEach(function(blank){
+    var userInput =  $("input#" + blank).val();
+    $("." + blank).text(userInput);
+    });
 
-    $(".person1").text(person1);
-    $(".person2").text(person2);
-    $(".codinglanguage").text(codinglanguage).val();
-    $(".exclamation").text(exclamation).val();
-    $(".verb").text(verb).val();
-    $(".noun").text(noun).val();
+    // var person1 = $("input#person1").val();
+    // var person2 = $("input#person2").val();
+    // var codinglanguage = $("input#codinglanguage").val();
+    // var exclamation = $("input#exclamation").val();
+    // var verb = $("input#verb").val();
+    // var noun = $("input#noun").val();
+    //
+    // $(".person1").text(person1);
+    // $(".person2").text(person2);
+    // $(".codinglanguage").text(codinglanguage).val();
+    // $(".exclamation").text(exclamation).val();
+    // $(".verb").text(verb).val();
+    // $(".noun").text(noun).val();
 
     $("#story").show();
 
